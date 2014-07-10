70d3eeb9f41d3f635f417041b1002ece767bf670
Jay Zoldak <zoldak@edx.org>
2014-07-10 09:19:26 -0400

    Add ability to save HAR file to disk

--- bok_choy/web_app_test.py
+++ bok_choy/web_app_test.py
@@ -2,6 +2,8 @@
 Base class for testing a web application.
 """
 import sys
+import os
+import json
 from unittest import TestCase
 from abc import ABCMeta
 from uuid import uuid4
@@ -27,19 +29,21 @@ class WebAppTest(TestCase):
         Returns:
             None
         """
+        # Start up the browsermob proxy so that we can save HAR file for profiling
         server = Server('browsermob-proxy')
         server.start()
-        proxy = server.create_proxy()
+        self.proxy = server.create_proxy()
 
         # If using SauceLabs, tag the job with test info
         tags = [self.id()]
 
         # Set up the page objects
         # This will start the browser, so add a cleanup
-        self.browser = browser(tags=tags, proxy=proxy)
+        self.browser = browser(tags=tags, proxy=self.proxy)
 
         # Cleanups are executed in LIFO order.
         # This ensures that the screenshot is taken BEFORE the browser quits.
+        self.addCleanup(server.stop)
         self.addCleanup(self.browser.quit)
         self.addCleanup(self._screenshot)
 
@@ -68,3 +72,22 @@ class WebAppTest(TestCase):
                 save_screenshot(self.browser, self.id())
             except:
                 pass
+
+    def save_har_file(self, har, name):
+        """
+        Save a HAR file.
+
+        The location of the har file can be configured
+        by the environment variable `HAR_DIR`.  If not set,
+        this defaults to the current working directory.
+
+        Args:
+            proxy: The browsermob proxy.
+            name (str): A name for the har file, which will be used in the output file name.
+
+        Returns:
+            None
+        """
+        har_file = os.path.join(os.environ.get('HAR_DIR', ''), '{}.har'.format(name))
+        with open(har_file, 'w') as output_file:
+            json.dump(har, output_file)
